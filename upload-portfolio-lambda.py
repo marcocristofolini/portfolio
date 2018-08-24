787f2cda535191ff58ead7a2fe5d80669394d1ef
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-08-24 19:27:21 -0300

    adjusts

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,8 +4,8 @@ import zipfile
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-build_bucket = s3.Bucket('portfoliobuild.robinnorwood.info')
-portfolio_bucket = s3.Bucket('portfolio.robinnorwood.info')
+build_bucket = s3.Bucket('portfoliobuild.tudosobre.cloud')
+portfolio_bucket = s3.Bucket('portfolio.tudosobre.cloud')
 
 # On Windows, this will need to be a different location than /tmp
 build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
