ae3bff0ca3d1aded02b70faef65f82661fe6d0d1
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-08-24 20:58:07 -0300

    fix upload script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,16 +5,15 @@ import mimetypes
 
 s3 = boto3.resource('s3')
 
-build_bucket = s3.Bucket('portfoliobuild.tudosobre.cloud')
 portfolio_bucket = s3.Bucket('portfolio.tudosobre.cloud')
+build_bucket = s3.Bucket('portfoliobuild.tudosobre.cloud')
 
 portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm
+        portfolio_bucket.upload_fileobj(obj, nm,
             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
