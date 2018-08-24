b62a4017b1afe1171fc2d10804a0ea9911bef6b8
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-08-24 20:50:26 -0300

    teste

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,17 +1,20 @@
 import boto3
-from botocore.client import Config
+import StringIO
 import zipfile
+import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+s3 = boto3.resource('s3')
 
 build_bucket = s3.Bucket('portfoliobuild.tudosobre.cloud')
 portfolio_bucket = s3.Bucket('portfolio.tudosobre.cloud')
 
-# On Windows, this will need to be a different location than /tmp
-build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        target_bucket.upload_fileobj(obj, nm)
-        target_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket.upload_fileobj(obj, nm
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
