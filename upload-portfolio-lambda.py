83014e016707d81ccc8ec05ba4b2d70ddf44faf7
Marco Cristofolini <mf.cristofolini@gmail.com>
2018-08-24 22:57:20 -0300

    fix

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,30 +1,32 @@
 import boto3
 import StringIO
 import zipfile
+import mimetypes
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:423733455502:deployPortfolioTopic')
-    
+
     try:
         s3 = boto3.resource('s3')
-        
+
         portfolio_bucket = s3.Bucket('portfolio.tudosobre.cloud')
         build_bucket = s3.Bucket('portfoliobuild.tudosobre.cloud')
-        
+
         portfolio_zip = StringIO.StringIO()
         build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-        
+
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        
+
         print "Job Done"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
         raise
-    
+
     return "Hello from lambda"
